631113aca6dee0c0bbc64128c17f361aa28b1423
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-01-25 22:46:31 +0500

    weather app

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const changeLocation = document.getElementById('change-location')
+const card = document.getElementById('card')
+const details = document.getElementById('details')
+const weatherIcon = document.getElementById('weather-icon')
