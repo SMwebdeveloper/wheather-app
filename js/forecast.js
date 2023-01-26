f02f21ad76053798c773343c34e592d802b8b11f
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-01-26 15:23:00 +0500

    fetch api

--- js/forecast.js
+++ js/forecast.js
@@ -2,3 +2,15 @@
 // bu mening kalitim. Agar o'zingiz olsangiz va ishlamasa buni ishlating.
 // Agar ishlamay qolsa unda 1 soatda so'rovlar soni limitdan oshib ketgan bo'ladi.
 // Aloqa: Telegram => @akror_web
+
+const KEY = "85b27135db125a6bb2ab011772b77f99";
+
+const getData = async (city) => {
+  const base = "https://api.openweathermap.org/data/2.5/weather";
+  const query = `?q=${city}&units=metric&appid=${KEY}`;
+
+  const req = await fetch(base + query);
+  const data = await req.json();
+  console.log(data);
+  return data;
+};
